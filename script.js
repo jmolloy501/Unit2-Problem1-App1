4dccc940b92ea7ff97d95d59bad1ffb1b4c18e00
jmolloy501 <70666253+jmolloy501@users.noreply.github.com>
2021-02-17 15:43:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-require(["esri/views/MapView", "esri/WebMap"], function(MapView, WebMap) {
-        
-        var webmap = new WebMap({
-          portalItem: {
-            // autocasts as new PortalItem()
-            id: "58f20c17b62d474b87781ea7513ebc53"
-          }
-        });
-        
-  
-        var view = new MapView({
-          map: webmap,
-          container: "viewDiv"
-        });
-      });
+
